eaf9af98d38f66db6af535e3e3685622266a6b7a
Your Name <you@example.com>
2024-01-25 19:50:55 +0000

    comprehensive todo demo in single file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  state = { todos: ["Clean car", "Buy bread", "Feed fish"] };
+  state = {
+    todos: [
+      { title: "Clean car", done: false },
+      { title: "Clean bike", done: true },
+      { title: "Buy milk", done: false },
+    ],
+  };
 
   onTodoInput = (e) => {
     this.setState({ todoInput: e.target.value });
@@ -9,21 +16,57 @@ class App extends Component {
 
   onAddClick = () => {
     const todos = [...this.state.todos];
-    todos.push(this.state.todoInput);
+    todos.push({ title: this.state.todoInput, todo: false });
     this.setState({ todos });
+  };
+
+  onDeleteClick = (title) => {
+    const todos = [...this.state.todos];
+    const indexOf = todos.findIndex((todo) => todo.title === title);
+    console.log(indexOf);
+    todos.splice(indexOf, 1);
+    this.setState({ todos });
+  };
+
+  onTodoToggleClick = (title) => {
+    console.log(title);
+    const todos = [...this.state.todos];
+    const indexOf = todos.findIndex((todo) => todo.title === title);
+    todos[indexOf].done = !todos[indexOf].done; // makes done opposite of itself
+    this.setState({ todos });
+  };
+
+  onDeleteAll = () => {
+    this.setState({ todos: [] });
   };
 
   render() {
     console.log(this.state);
     const { todos } = this.state;
 
+    let totalTodo = 0;
+    todos.forEach((todo) => {
+      if (!todo.done) {
+        totalTodo++;
+      }
+    });
+
     return (
       <>
         <input type="text" onInput={this.onTodoInput} />
         <button onClick={this.onAddClick}>Add</button>
+        <h1>Total todos: {totalTodo}</h1>
         {todos.map((todo) => {
-          return <p>{todo}</p>;
+          return (
+            <div className={todo.done ? "done" : "undone"}>
+              <p onClick={() => this.onTodoToggleClick(todo.title)}>
+                {todo.title}
+              </p>
+              <button onClick={() => this.onDeleteClick(todo.title)}>X</button>
+            </div>
+          );
         })}
+        <button onClick={this.onDeleteAll}>Delete ALL</button>
       </>
     );
   }
